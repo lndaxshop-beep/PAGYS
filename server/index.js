997e3956501f7dc2dbc41325981769878c730993
Abraham <k7mensah7@gmail.com>
2026-05-27 10:17:07 +0000

    fix: strip quotes from Firebase private key for Hostinger compat

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ try {
         credential: admin.credential.cert({
           projectId: process.env.FIREBASE_PROJECT_ID,
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/^["']|["']$/g, '').replace(/\\n/g, '\n'),
         }),
       });
     } else {
